e43bf28523148ce723029279d62936d8b6287eef
Ivan Rodriguez <sbr.com@hotmail.com>
2021-10-30 07:04:54 -0500

    adding more test

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -3,8 +3,19 @@ pragma solidity >=0.4.21 <0.9.0;
 
 contract Fundraiser {
   string public name;
+  string public url;
+  string public imageURL;
+  string public description;
 
-  constructor(string memory _name) public {
+  constructor(
+    string memory _name,
+    string memory _url,
+    string memory _imageURL,
+    string memory _description
+  ) public {
     name = _name;
+    url = _url;
+    imageURL = _imageURL;
+    description = _description;
   }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -3,16 +3,40 @@ const FundraiserContract = artifacts.require("Fundraiser");
 contract("Fundraiser", accounts => {
   let fundraiser;
   const name = "Beneficiary Name"
+  const url = "beneficiaryname.org"
+  const imageURL = "https://placekitten.com/600/350"
+  const description = "Beneficiary description"
 
   describe("initialization", () => {
 
     beforeEach(async () => {
-      fundraiser = await FundraiserContract.new(name)
+      fundraiser = await FundraiserContract.new(
+        name,
+        url,
+        imageURL,
+        description
+      )
     })
 
     it("gets beneficiary name", async () => {
       const actual = await fundraiser.name()
       assert.equal(actual, name, "names should match")
+    })
+
+    it("gets beneficiary url", async () => {
+      const actual = await fundraiser.url()
+      assert.equal(actual, url, "url should match")
+    })
+
+
+    it("gets beneficiary imageURL", async () => {
+      const actual = await fundraiser.imageURL()
+      assert.equal(actual, imageURL, "imageURL should match")
+    })
+
+    it("gets beneficiary description", async () => {
+      const actual = await fundraiser.description()
+      assert.equal(actual, description, "names should match")
     })
   })
 })
