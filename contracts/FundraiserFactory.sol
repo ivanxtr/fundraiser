ae4dc0ab5bf88663f0f63c5cb0cbfd962d87539d
Ivan Rodriguez <sbr.com@hotmail.com>
2021-11-13 22:25:01 -0600

    latest changes to fundraisers

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -4,11 +4,10 @@ pragma solidity >=0.4.21 <0.9.0;
 import "./Fundraiser.sol";
 
 contract FundraiserFactory {
+  uint256 constant maxLimit = 20;
   Fundraiser[] private _fundraisers;
 
-  function fundraisersCount() public view returns(uint256) {
-    return _fundraisers.length;
-  }
+  event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
 
   function createFundraiser(
     string memory name,
@@ -26,5 +25,26 @@ contract FundraiserFactory {
       msg.sender
     );
     _fundraisers.push(fundraiser);
+    emit FundraiserCreated(fundraiser, beneficiary);
+  }
+
+  function fundraisersCount() public view returns(uint256) {
+    return _fundraisers.length;
+  }
+
+  function fundraisers(uint256 limit, uint256 offset)
+    public view returns(Fundraiser[] memory coll) {
+      require(offset <= fundraisersCount(), "offset out of bounds");
+
+      uint256 size = fundraisersCount() - offset;
+      size = size < limit ? size: limit;
+      size = size < maxLimit ? size : maxLimit;
+      coll = new Fundraiser[](size);
+
+      for(uint256 i = 0; i < size; i++) {
+        coll[i] = _fundraisers[offset + i];
+      }
+
+    return coll;
   }
 }
